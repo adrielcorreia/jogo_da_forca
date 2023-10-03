df43572ffef0fa4524eecc74a63f77905d8f49ce
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-03 15:18:20 -0300

    some-changes

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 let keyboard = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z'];
+keyboard = keyboard.map((e) => {
+    return e.toUpperCase();
+})
 
 const ul = document.querySelector('.keyboard')
 const ulTeste = document.querySelector('.test')
-let word = document.querySelector('.word').innerText
 const lifesText = document.querySelector('.lifes')
 let lifes = 5; lifesText.innerHTML = `Tentativas: ${lifes}`
-
 const lose = document.querySelector('.lose')
-
-const guess_word = 'BICICLE'
+const guess_word = 'bicicleta'.toUpperCase()
 const guess_word_split = guess_word.split('')
+console.log(lifesText)
 
 for (const i of guess_word_split) {
     ulTeste.innerHTML += `<li class="lett" data-letter>${i}</li>`
 }
-
-console.log(guess_word_split)
-
-keyboard = keyboard.map((e) => {
-    return e.toUpperCase();
-})
 
 const Letters = document.querySelectorAll('[data-letter]')
 
@@ -31,10 +26,12 @@ Letters.forEach((e) => {
 console.log(Letters)
 
 for (i = 0; i < keyboard.length; i++) {
-    ul.innerHTML += `<li class="letter"> <button data-btn>${keyboard[i]}</button> </li>`
+    ul.innerHTML += `<li class="letter" ><button data-btn>${keyboard[i]}</button></li>`
 }
 
 const btn = document.querySelectorAll('[data-btn]')
+const li = document.querySelectorAll('.letter')
+li[18].style.gridColumn = '2'
 
 btn.forEach(e => {
     e.addEventListener('click', () => {
