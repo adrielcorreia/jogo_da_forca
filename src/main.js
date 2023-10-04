45d0672f2bf1df26aeb2993483fef77270c0a48e
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-04 12:48:49 -0300

    almost-done

--- src/main.js
+++ src/main.js
@@ -4,19 +4,23 @@ keyboard = keyboard.map((e) => {
     return e.toUpperCase()
 })
 
+const guess_word = generate_random().toUpperCase()
 
 const ul = document.querySelector('.keyboard')
 const word = document.querySelector('.word')
 const lifes = document.querySelector('.lifes')
 const lose = document.querySelector('.lose')
-const guess_word = 'bicicleta'.toUpperCase()
 const gws = guess_word.split('') // guess word split
 const img = document.querySelector('.hangman')
+const all = document.querySelector('.all')
+const menu = document.querySelector('.menu')
+const correct_answer = document.querySelector('.corrent_answer')
+correct_answer.innerText = `The correct word was ${guess_word}`
 
-lifes.innerText = `Tentativas: ${lifes.dataset.life}/6`
+lifes.innerText = `Incorrent guesses: ${lifes.dataset.life}/6`
 
 for (const i of gws) {
-    word.innerHTML += `<li class="lett" ><p data-letter>${i}</p><div class="underline" data-underline>${i}</div></li>`
+    word.innerHTML += `<li class="lett" ><p data-letter>&nbsp</p><div class="underline" data-underline>${i}</div></li>`
 }
 
 for (i = 0; i < keyboard.length; i++) {
@@ -25,7 +29,6 @@ for (i = 0; i < keyboard.length; i++) {
 
 const Letters = document.querySelectorAll('[data-letter]')
 const Underlines = document.querySelectorAll('[data-underline]')
-
 const button = document.querySelectorAll('[data-btn]')
 let count = 0
 
@@ -37,37 +40,49 @@ console.log(filter)
 
 button.forEach(btn => {
     btn.addEventListener('click', () => {
-        if (count == filter.length) return
-
-        if (lifes.dataset.life < 6) {
+        if (lifes.dataset.life < 5) {
             btn.style.cursor = 'default'
+
+            for (i = 0; i < gws.length; i++) {
+                if (btn.innerText == gws[i]) {
+                    console.log(btn.innerText);
+                    Letters[i].innerText = gws[i]
+                    Letters[i].classList.add('w_guessed') //word guessed
+                    Underlines[i].classList.add('u_guessed')
+                    btn.style.background = '#9FA1D6'
+                }
+            }
             
             if (gws.includes(btn.innerText)) {
                 count++
-                btn.style.background = '#9FA1D6'
-
-                Letters.forEach((letter) => {
-                    if (letter.innerText == btn.innerText) {
-                        letter.classList.add('w_guessed') //word guessed
-                    }
-                })
-
-                Underlines.forEach((underline) => {
-                    if (underline.innerText == btn.innerText) {
-                        underline.classList.add('u_guessed') //underline guessed
-                    }
-                })
-                console.log(count)
+                if (count == filter.length) {
+                    console.log('win')
+                    const wl_img = document.querySelector('.wl_img')
+                    wl_img.src = './assets/victory.gif'
+                    menu.style.visibility = 'visible'
+                    menu.style.opacity = '1'
+                    menu.style.background = 'background: rgba(0, 0, 0, .5);'
+                    const wl = document.querySelector('.wl')
+                    wl.innerText = 'You Win!'
+                    return
+                }
 
             } else {
                 lifes.dataset.life++
-                lifes.innerText = `Tentativas: ${lifes.dataset.life}/6`
+                lifes.innerText = `Incorrent guesses: ${lifes.dataset.life}/6`
                 img.src = `./assets/hangman-${lifes.dataset.life}.svg`
-                btn.style.background = '#ba5e5e'
+                btn.style.background = 'var(--error-color)'
             }
         } else {
-            //lifes.innerText = `Tentativas: 0`
-            lose.style.visibility = 'visible'
+            const wl = document.querySelector('.wl')
+            wl.innerText = 'Game Over!'
+            lifes.dataset.life++
+            lifes.innerText = `Incorrect guesses: ${lifes.dataset.life}/6`
+            img.src = `./assets/hangman-${lifes.dataset.life}.svg`
+            menu.style.visibility = 'visible'
+            menu.style.opacity = '1'
+            menu.style.background = 'background: rgba(0, 0, 0, .5);'
+            return
         }
         
     }, {once: true});
