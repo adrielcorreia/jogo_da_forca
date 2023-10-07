1b2b26a142abecd2c538ac3389e3ede33786ee55
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-07 00:30:04 -0300

    reajust export and import

--- src/js/api_request.js
+++ src/js/api_request.js
@@ -1,16 +1,23 @@
 var init = {
     method: "GET",
     headers: {'X-Api-Key': '8B6D5CFzigYLTX2jUNJgug==h5OleVcXvOSVVilk'},
-};
+}
+
 export async function fetchTip(animal) {
     const api = `https://api.api-ninjas.com/v1/animals?name=${animal}`
+    let name = ''
     let slogan = ''
     
     await fetch(api, init).then((res) => res.json().then(data => {
         slogan = data[0].characteristics.slogan
+        name = data[0].name
         
-    }).catch((err) => {
-        console.log('Ocorreu um erro durante a execução do fetch:', err)
+    }).catch(() => {
+        if (name == '' || name == undefined) {
+            console.log(`animal inexistente`)
+        } else {
+            console.log('deu erro na hora do fetch')
+        }
     }))
 
     return slogan
