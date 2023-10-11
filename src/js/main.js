f6b9ab526aa69a1e5f69fd3cb1a5bb7f10275b19
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-11 12:36:33 -0300

    some-changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 // import section
-import { fetchTip } from './api_request.js'
 import { random } from './random.js'
 
 const ul = document.querySelector('.keyboard')
@@ -11,21 +10,16 @@ const wl_img = document.querySelector('.wl_img')
 const wl = document.querySelector('.wl')
 const answer = document.querySelector('.corrent_answer')
 const menu = document.querySelector('.menu')
-
-const gssWord = random()
+const randomAnimal = random()
+const gssWord = randomAnimal.nome
 const gws = gssWord.toUpperCase().split('') // guess word split
+const gssAccents = (gssWord.normalize('NFD').replace(/[\u0300-\u036f]/g, '')).toUpperCase().split('');
+const filter = new Set(gssAccents)
 let keyboard = []
 
-fetchTip(gssWord).then((slogan) => {
-    tip.innerText = slogan
-})
-
-lifes.innerText = `Incorrent guesses: ${lifes.dataset.life}/6`
-
 for (let i = 65; i < 91; i++) {keyboard.push(String.fromCharCode(i))}
-
 for (const i of gws) {word.innerHTML += `<li class="lett" ><p data-letter>&nbsp</p><div class="underline" data-underline></div></li>`}
-for (const i of keyboard) {ul.innerHTML += `<li class="letter"><button data-btn>${i}</button></li>`}
+for (const i of keyboard) {ul.innerHTML += `<li class="letter"><button data-btn="${i}">${i}</button></li>`}
 
 const Letters = document.querySelectorAll('[data-letter]')
 const Underlines = document.querySelectorAll('[data-underline]')
@@ -33,9 +27,10 @@ const button = document.querySelectorAll('[data-btn]')
 const letter = document.querySelectorAll('.letter')
 let count = 0
 
+wl_img.style.width = '45%'
+tip.innerText = randomAnimal.dica
+lifes.innerText = `Tentativas: ${lifes.dataset.life}/6`
 letter[20].style.gridColumn = '3'
-
-let filter = new Set(gws)
 
 function final(msg, img) {
     wl_img.src = `./assets/${img}.gif`
@@ -43,8 +38,9 @@ function final(msg, img) {
     menu.style.visibility = 'visible'
     menu.style.opacity = '1'
     menu.style.background = 'background: rgba(0, 0, 0, .5);'
-    answer.innerText = `The correct word was ${gssWord}`
-    return
+    if (msg == 'Você perdeu!') {
+        answer.innerText = `A palavra era ${gssWord}`
+    }
 }
 
 function guessed(i) {
@@ -55,21 +51,21 @@ function guessed(i) {
 
 function not_guessed() {
     lifes.dataset.life++
-    lifes.innerText = `Incorrent guesses: ${lifes.dataset.life}/6`
+    lifes.innerText = `Tentativas: ${lifes.dataset.life}/6`
     img.src = `./assets/hangman-${lifes.dataset.life}.svg`
 }
 
 for (let i = 0; i < gws.length; i++) {
-    if (gws[i] == ' ') {
+    if (gws[i] == '-') {
         count++
         Underlines[i].classList.add('u_guessed')
+        Letters[i].classList.add('w_guessed')
     }
 }
 //---------------------------------------------------------------------------
 button.forEach(btn => {
     document.addEventListener('keydown', (e) => {
-        var name = e.key.toUpperCase()
-        if (name == btn.innerText) {
+        if (e.key.toUpperCase() == btn.dataset.btn) {
             btn.click()
         }
     }, false)
@@ -78,27 +74,26 @@ button.forEach(btn => {
         if (lifes.dataset.life < 5) {
             btn.style.cursor = 'default'
             
-            if (gws.includes(btn.innerText)) {
+            if (gssAccents.includes(btn.innerText)) {
                 count++
                 if (count == filter.size) {
-                    final('You Win!', 'victory')
-                    wl_img.style.width = '45%'
+                    final('Você ganhou!', 'victory')
                 }
 
                 for (let i = 0; i < gws.length; i++) {
-                    if (btn.innerText == gws[i]) {
+                    if (btn.dataset.btn == gssAccents[i]) {
                         guessed(i)
                         btn.style.background = '#9FA1D6'
                     }
                 }
+
             } else {
                 not_guessed()
                 btn.style.background = 'var(--error-color)'
             }
         } else {
             not_guessed()
-            final('Game Over!', 'lost')
-            wl_img.style.width = '45%'
+            final('Você perdeu!', 'lost')
         }
     }, {once: true});
-});
+})
--- src/js/random.js
+++ src/js/random.js
@@ -1,22 +1,208 @@
-const animals = [
-    'Bengar Tiger', 'Elephant', 'Giraffe', 
-    'Kangaroo', 'Zebra', 'Rhinoceros', 
-    'Panda', 'Hippopotamus', 'Koala', 
-    'Gorilla', 'Penguin', 'Shark', 
-    'Owl', 'Chameleon', 'Dolphin', 
-    'Cheetah', 'Polar Bear', 'Ostrich', 
-    'Raccoon', 'Sloth', 'Hedgehog', 
-    'Jaguar', 'Armadillo', 'Meerkat', 
-    'Gibbon', 'Mongoose', 'Chinchilla', 
-    'Hummingbird', 'Koala', 'Wallaby', 
-    'Hornet', 'Jackal', 'Koala', 'Macaw', 
-    'Nightingale', 'Ocelot', 'Platypus', 
-    'Quokka', 'Uakari', 'Weasel', 
-    'Yak', 'Zonkey', 'Alligator', 'Baboon', 
-    'Coyote', 'Dingo', 'Elephant Seal', 'Hippopotamus', 
-    'Kingfisher', 'Numbat', 'Ox', 'Peacock', 'Quokka', 'Raccoon Dog', 'Tarsier', 'Umbrellabird', 'Vampire Bat', 'Zorse'
+const animais = [
+    { 
+        nome: "gato", 
+        dica: "É um animal de estimação com quatro patas." 
+    
+    },
+    { 
+        nome: "cachorro", 
+        dica: "É conhecido como o melhor amigo do homem." 
+    
+    },
+    { 
+        nome: "elefante", 
+        dica: "É o maior animal terrestre e tem uma longa tromba." 
+    },
+    { 
+        nome: "girafa", 
+        dica: "Tem um pescoço muito longo e manchas irregulares em seu corpo." 
+    },
+    { 
+        nome: "leão", 
+        dica: "É conhecido como o rei da selva." 
+    },
+    { 
+        nome: "tigre", 
+        dica: "É um grande felino com listras em seu corpo." 
+    },
+    { 
+        nome: "urso", 
+        dica: "É um animal grande e peludo que hiberna no inverno." 
+    },
+    { 
+        nome: "macaco", 
+        dica: "É um animal inteligente que vive em árvores e tem um rabo longo." 
+    },
+    { 
+        nome: "pássaro", 
+        dica: "Tem penas e geralmente voa no céu." 
+    },
+    { 
+        nome: "peixe", 
+        dica: "Vive na água e respira através de brânquias." 
+    },
+    { 
+        nome: "baleia", 
+        dica: "É o maior mamífero marinho." 
+    },
+    { 
+        nome: "cobra", 
+        dica: "É um réptil sem pernas que rasteja no chão." 
+    },
+    { 
+        nome: "jacaré", 
+        dica: "É um réptil semelhante ao crocodilo." 
+    },
+    { 
+        nome: "hipopótamo", 
+        dica: "É um animal grande que vive na água e em terra." 
+    },
+    { 
+        nome: "zebra", 
+        dica: "Tem listras pretas e brancas em seu corpo." 
+    },
+    { 
+        nome: "coruja", 
+        dica: "É uma ave noturna conhecida por sua sabedoria." 
+    },
+    { 
+        nome: "panda", 
+        dica: "É um urso preto e branco originário da China." 
+    },
+    { 
+        nome: "esquilo", 
+        dica: "É um roedor fofo que gosta de nozes." 
+    },
+    { 
+        nome: "borboleta", 
+        dica: "É um inseto colorido que voa durante o dia." 
+    },
+    { 
+        nome: "abelha", 
+        dica: "É um inseto que produz mel e pica quando se sente ameaçado." 
+    },
+    { 
+        nome: "aranha", 
+        dica: "Tem oito patas e tece teias para pegar insetos." 
+    },
+    { 
+        nome: "lobo", 
+        dica: "É um animal selvagem que vive em matilhas." 
+    },
+    { 
+        nome: "rinoceronte", 
+        dica: "É um animal grande com um chifre no nariz." 
+    },
+    { 
+        nome: "hipogrifo", 
+        dica: "É uma criatura mítica com corpo de cavalo e asas de águia." 
+    },
+    { 
+        nome: "unicornio", 
+        dica: "É uma criatura mítica com um chifre na testa." 
+    },
+    { 
+        nome: "salamandra", 
+        dica: "É um anfíbio que pode regenerar partes do seu corpo." 
+    },
+    { 
+        nome: "tartaruga", 
+        dica: "Tem um casco duro que a protege." 
+    },
+    { 
+        nome: "lagarto", 
+        dica: "É um réptil pequeno que pode soltar sua cauda quando ameaçado." 
+    },
+    { 
+        nome: "pinguim", 
+        dica: "É uma ave marinha que não voa e vive no Polo Sul." 
+    },
+    { 
+        nome: "águia", 
+        dica: "É uma grande ave de rapina com visão aguçada." 
+    },
+    { 
+        nome: "papagaio", 
+        dica: "É uma ave colorida que pode imitar sons." 
+    },
+    { 
+        nome: "hiena", 
+        dica: "É um animal selvagem conhecido por sua risada peculiar." 
+    },
+    { 
+        nome: "foca", 
+        dica: "É um mamífero marinho que vive na água e na terra." 
+    },
+    { 
+        nome: "lontra", 
+        dica: "É um animal aquático conhecido por sua fofura e agilidade." 
+    },
+    { 
+        nome: "morsa", 
+        dica: "É um mamífero marinho grande com presas longas." 
+    },
+    { 
+        nome: "texugo", 
+        dica: "É um mamífero que vive em tocas subterrâneas." 
+    },
+    { 
+        nome: "javali", 
+        dica: "É um porco selvagem que vive em florestas." 
+    },
+    { 
+        nome: "avestruz", 
+        dica: "É uma ave grande que não voa e tem pernas longas." 
+    },
+    { 
+        nome: "flamingo", 
+        dica: "É uma ave de pernas longas com penas rosadas." 
+    },
+    { 
+        nome: "rinoceronte", 
+        dica: "É um animal grande com um chifre no nariz." 
+    },
+    { 
+        nome: "suricate", 
+        dica: "É um pequeno mamífero conhecido por ficar de pé nas patas traseiras." 
+    },
+    { 
+        nome: "guaxinim", 
+        dica: "É um mamífero com uma máscara facial e uma cauda listrada." 
+    },
+    { 
+        nome: "búfalo", 
+        dica: "É um mamífero herbívoro grande com chifres curvos." 
+    },
+    { 
+        nome: "antílope", 
+        dica: "É um mamífero ágil conhecido por seus saltos impressionantes." 
+    },
+    { 
+        nome: "cavalo", 
+        dica: "É um animal domesticado usado para corridas e transporte." 
+    },
+    { 
+        nome: "ovelha", 
+        dica: "É um animal de fazenda conhecido por sua lã macia." 
+    },
+    { 
+        nome: "falcão", 
+        dica: "É uma ave de rapina rápida e ágil." 
+    },
+    { 
+        nome: "beija-flor", 
+        dica: "É a menor ave do mundo, conhecida por sua rapidez nas asas." 
+    },
+    { 
+        nome: "golfinho", 
+        dica: "É um mamífero marinho inteligente e amigável." 
+    },
+    { 
+        nome: "jaguar", 
+        dica: "É um grande felino com manchas em forma de rosas." 
+    }
 ]
-
+  
 export function random() {
-    return animals[Math.floor(Math.random()*animals.length)]
+    return animais[Math.floor(Math.random()*animais.length)]
 }
